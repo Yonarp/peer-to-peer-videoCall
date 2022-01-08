3f12e2f9ef300110a33ca895b533272bc3428736
Pranoy Sarkar <pranoy0211@gmail.com>
2022-01-09 02:46:34 +0530

    added input field for name on landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import "./LandingPage.scss"
+import React from "react";
+import "./LandingPage.scss";
+import TextField from "@material-ui/core/TextField";
 
 function LandingPage() {
-    return (
-        <div className="landing-page">
-        <div className="landing-page-heading">
-            Get started with your calls
-        </div>
-        <div className="landing-page-para">
-            Our calls are peer to peer and smoother than average video calls that you do at home.
-        </div>
-        </div>
-    )
+  return (
+    <div className="landing-page">
+      <div className="landing-page-heading">Get started with your calls</div>
+      <div className="landing-page-para">
+        Our calls are peer to peer and smoother than average video calls that
+        you do at home.
+      </div>
+      <div className="landing-page-text">
+        <input type="text" className="landing-page-input" required></input>
+        <span className="landing-page-placeholder"> Enter Name</span>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
